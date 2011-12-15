46502d4b3e934fb727159ba1787fb1a83ef72218
Kamen Naydenov <pau4o@kamennn.eu>
2011-12-15 14:04:05 +0200

    fast gettext config
    added documentation for settings

---
+++ config/initializers/fast_gettext.rb
@@ -0,0 +1,3 @@
+FastGettext.add_text_domain 'pilotorder', :path => 'locale', :type => :po
+FastGettext.default_available_locales = ['en','bg'] #all you want to allow
+FastGettext.default_text_domain = 'pilotorder'
